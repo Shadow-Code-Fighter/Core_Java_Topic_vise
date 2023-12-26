e12bb45981eb51289a686334fd05c86911d74781
Gaurav Sharma <kgauravs192@gmail.com>
2023-12-27 01:14:13 +0530

    created

---
+++ Basic_code_java/Finalize.java
@@ -0,0 +1,14 @@
+package Basic_code_java;
+
+public class Finalize {
+    public static void main(String []args){
+        Finalize fz =  new Finalize();
+        fz=null; //Colling garbage collector
+        System.gc();
+        System.out.println("End");
+    }
+    @Override
+    protected void finalize(){
+        System.out.println("Finalize method called");
+    }
+}
