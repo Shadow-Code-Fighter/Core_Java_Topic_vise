8a3779b18498a494737a8f263f4c1fd8cabea24b
Gaurav Sharma <kgauravs192@gmail.com>
2024-01-02 00:09:58 +0530

    added

--- Interview_code_QP/reverses_String.java
+++ Interview_code_QP/reverses_String.java
@@ -1,5 +1,15 @@
 package Interview_code_QP;
 
+import java.util.Scanner;
+
 public class reverses_String {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter words");
+        String str = sc.nextLine();
+
+        for(int i=str.length()-1;i>=0;i--){
+            System.out.print(str.charAt(i));
+        }
+    }
 }
