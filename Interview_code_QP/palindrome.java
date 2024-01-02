d0cb02edbd7c2568f5d5fc63cda5f12bde2da188
Gaurav Sharma <kgauravs192@gmail.com>
2024-01-02 17:56:45 +0530

    added

---
+++ Interview_code_QP/palindrome.java
@@ -0,0 +1,15 @@
+package Interview_code_QP;
+
+import java.util.Scanner;
+
+public class palindrome {
+
+    public static void main(String[] args) {
+        String original ,reverse="";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a String to check if it is a pelidrome: ");
+        
+        
+    }
+    
+}
