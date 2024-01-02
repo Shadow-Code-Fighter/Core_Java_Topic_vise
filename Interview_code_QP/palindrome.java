81f906f2fc211839241321915704f8af793ab80e
Gaurav Sharma <kgauravs192@gmail.com>
2024-01-02 18:01:23 +0530

    updated

--- Interview_code_QP/palindrome.java
+++ Interview_code_QP/palindrome.java
@@ -12,7 +12,15 @@ public class palindrome {
 
         int length = original.length();
         System.out.println(length);
-        
+
+        for(int i=length-1;i>=0;i++){
+            reverse=reverse+original.charAt(i);
+        }
+        if(original.equals(reverse)){
+            System.out.println("Entered string is a pelidrome");
+        }else{
+            System.out.println("Entered string is not pelidrome");
+        }
     }
     
 }
