a236a5bfd952eb6a7daa5cadb2105f5a4b4f4f09
Gaurav Sharma <kgauravs192@gmail.com>
2024-01-06 15:51:09 +0530

    added Custom Exception program

---
+++ Basic_code_java/CustomException.java
@@ -0,0 +1,13 @@
+package Basic_code_java;
+
+//User create custom exception where inherit parent message using super keyword
+class UserCustomException extends Exception {
+    public UserCustomException(String message){
+        super(message);
+    }
+}
+public class CustomException {
+    public static void main(String[] args) throws UserCustomException {
+        throw new UserCustomException("This is my Custom Exception");
+    }
+}
