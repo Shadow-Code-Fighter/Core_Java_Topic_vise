90f9d898983e9ae3d723bc8443fb8218c78c2867
Gaurav Sharma <kgauravs192@gmail.com>
2024-01-09 23:23:36 +0530

    updated

--- Basic_code_java/StreamApi/StreamDuplicate.java
+++ Basic_code_java/StreamApi/StreamDuplicate.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Java8CodeStream {
+public class StreamDuplicate {
     
     public static void main(String[] args) {
         List<String> listData = Arrays.asList("Java","Python","C#","Kotlin","Java","Python");
