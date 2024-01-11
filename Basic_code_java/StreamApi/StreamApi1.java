6e32467b831df9a2f1b1f4c5ae5196c790d47c61
Gaurav Sharma <kgauravs192@gmail.com>
2024-01-11 16:00:14 +0530

    Added file,folder and updated

--- Basic_code_java/StreamApi/StreamApi1.java
+++ Basic_code_java/StreamApi/StreamApi1.java
@@ -2,12 +2,18 @@ package Basic_code_java.StreamApi;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.stream.Collector;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
 
 public class StreamApi1 {
     public static void main(String[] args) {
-        List<Integer> myList =  Arrays.asList(12,22,76,3,33,23);
+        List<Integer> myList =  Arrays.asList(12,3,76,22,33,23);
         myList.stream().filter(n->n%2==0).forEach(System.out::println);
+
+        // Consumer<Integer> c = n->System.out.println(myList);
+        // myList.forEach(c);
 
     }
 }
--- Interview_code_QP/palindrome.java
+++ Interview_code_QP/palindrome.java
@@ -2,17 +2,22 @@ package Interview_code_QP;
 
 import java.util.Scanner;
 
+//In this i have created two way to check pelindrome, in first way takes input as a String check it to as a String
+// In second way i have created to check pelindrome, in this method we have take an Integer and convert into String and
+// check true and false, output produces
+
 public class Palindrome {
 
     public static void main(String[] args) {
+
+        // first way
         String original ,reverse="";
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a String to check if it is a pelidrome: ");
         original=sc.nextLine();
 
-        int length = original.length();
-        System.out.println(length);
-
+        int length = original.length();                                
+        System.out.println(length);  
         for(int i=length-1;i>=0;i--)
             reverse=reverse+original.charAt(i);
         if(original.equals(reverse)){
@@ -21,6 +26,28 @@ public class Palindrome {
             System.out.println("Entered string is not pelidrome");
         }
         sc.close();
+
+        int number=121;       //----------------------------------------------
+         if (isPalindrome(number)) {                                        //
+            System.out.println(number + " is a palindrome.");               //
+        } else {                                                            //      Second way
+            System.out.println(number + " is not a palindrome.");           //
+        }       //------------------------------------------------------------                                                            // 
+
     }
+
+    // second way 
+    public static boolean isPalindrome(int x){ //-----------------------
+            String str = Integer.toString(x);                         //
+            int i = 0, j = str.length() - 1;                          //
+            while (i < j) {                                           //
+                if (str.charAt(i) != str.charAt(j)) {                 //
+                    return false;                                     //  Second way
+                }                                                     //
+                i++;                                                  //
+                j--;                                                  //
+            }                                                         //
+            return true;                                              //
+    }   //--------------------------------------------------------------
     
 }
