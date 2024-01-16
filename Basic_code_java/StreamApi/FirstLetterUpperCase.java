84d23752077367a46971b7435db2a7def97df644
Gaurav Sharma <kgauravs192@gmail.com>
2024-01-16 10:43:34 +0530

    updated

--- Basic_code_java/StreamApi/FirstLetterUpperCase.java
+++ Basic_code_java/StreamApi/FirstLetterUpperCase.java
@@ -1,14 +1,15 @@
 package Basic_code_java.StreamApi;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class FirstLetterUpperCase {
         public static void main(String[] args) {
-            // String input = "gaurav";
+            List<String> input = Arrays.asList("gaurav","rohan","ram");
     
-            String result = Arrays.stream("gaurav".split(" "))
-                    .map(s -> s.length() > 0 ? s.substring(0, 1).toUpperCase() + s.substring(1) : s)
+            String result = input.stream().map(s -> s.length() > 0 ? s.substring(0, 
+                    1).toUpperCase() + s.substring(1) : s)
                     .collect(Collectors.joining());
     
             System.out.println(result);
