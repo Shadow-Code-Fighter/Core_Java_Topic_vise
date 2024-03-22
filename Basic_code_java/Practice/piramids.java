975703a2f7d4c35bbaa11a330c95319593147009
Gaurav Sharma <kgauravs192@gmail.com>
2024-03-22 10:30:28 +0530

    updated

---
+++ Basic_code_java/Practice/piramids.java
@@ -0,0 +1,17 @@
+package Practice;
+
+public class piramids {
+    public static void main(String[] args) {
+        int i,j,k=8;
+        for(i=0;i<5;i++){
+            for(j=0;j<k;j++){
+                System.out.print("");
+            }
+            k=j-2;
+            for(j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
