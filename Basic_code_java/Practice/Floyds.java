0ba96114f0933d8f79b81b57c188f1cffef58af9
Gaurav Sharma <kgauravs192@gmail.com>
2024-03-22 10:36:15 +0530

    updated

--- Basic_code_java/Practice/Floyds.java
+++ Basic_code_java/Practice/Floyds.java
@@ -1,5 +1,22 @@
 package Practice;
 
+import java.util.Scanner;
+
 public class Floyds {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows\n");
+
+        int rows=sc.nextInt();
+        System.out.println("Floyd's Triangke Generated");
+        int count=1;
+
+        for(int i=1;i<=rows;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(count+" ");
+                count++;
+            }
+            System.out.println();
+        }
+    }
 }
