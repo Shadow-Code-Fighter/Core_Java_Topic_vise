d3633dd876bc49fbac734d379f6d12828e2cfd89
Gaurav Sharma <kgauravs192@gmail.com>
2024-03-22 10:49:41 +0530

    updated

---
+++ Basic_code_java/Practice/PatternNumberSequence.java
@@ -0,0 +1,28 @@
+package Practice;
+
+public class PatternNumberSequence {
+    // public static void main(String[] args) {
+    //     int a = 3;
+    //     int b = 4;
+    //     int n = 8;
+
+    //     for(int i = 1;i<=n;i++){
+    //         int c=a+b;
+    //         System.out.print(a+" "+b+" "+c);
+    //         System.out.println(" ");
+    //         a=c;
+    //         b=b+1;
+    //     }
+    // }
+
+    public static void main(String[] args) {
+        int a=3,b=4,n=8;
+        for(int i=1;i<=n;i++){
+            int c=a+b;
+            System.out.print(a+" + "+b+" = "+c);
+            System.out.println(" ");
+            a=c;
+            b+=1;
+        }
+    }
+}
