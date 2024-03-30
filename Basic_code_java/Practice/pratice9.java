1fa8cf5feaca22f846f89bdf486a53ec6ecaa3bc
Gaurav Sharma <kgauravs192@gmail.com>
2024-03-31 04:18:33 +0530

    updated

---
+++ Basic_code_java/Practice/pratice9.java
@@ -0,0 +1,23 @@
+package Practice;
+
+public class pratice9 {
+    public static void main(String[] args) {
+        String str="gaurav";
+        boolean isPelindrome=true;
+
+
+        for(int i=0;i<=str.length()/2;i++){
+            if(str.charAt(i)!=str.charAt(str.length()-i-1)){
+                isPelindrome = false;
+                break;
+            }
+        }
+        if (isPelindrome) {
+            System.out.println("Pelindrome");
+        }else{
+
+            System.out.println("Not Pelindrome");
+        }
+    }
+    
+}
