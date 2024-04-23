0cef412e69853fb086320046a0e260a5120ba2bf
Gaurav Sharma <kgauravs192@gmail.com>
2024-04-23 09:52:11 +0530

    updated

---
+++ Basic_code_java/StreamApi/UsingMethodFindisEven.java
@@ -0,0 +1,13 @@
+package StreamApi;
+
+public class UsingMethodFindisEven {
+    public static void main(String[] args) {
+        
+    }
+
+
+    public static boolean isEven(int number) { 
+        return number % 2 == 0; 
+    }
+    
+}
